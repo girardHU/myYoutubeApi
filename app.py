38feb6aa444e61c913ac5c76685c59b8480c66ec
girard_h <girard_h@etna-alternance.net>
2020-06-01 11:57:15 +0200

    added error on user post and regex check for params. TODO : encrypting password

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import json
+import re
 from flask import Flask
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
+from objects import Retour
 
+## REGEX
+word = re.compile('[a-zA-Z0-9_-]')
+email = re.compile('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$')
+
+## APP
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user@localhost:3306/mydb'
 db = SQLAlchemy(app)
 
 ## MODELS
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(45), unique=True, nullable=False)
@@ -23,25 +29,29 @@ class User(db.Model):
 
 @app.route('/')
 def hello_world():
-    user = User(username='premierTest', email='premierUser@test.fr', password='12345678')
-    db.session.add(user)
-    db.session.commit()
     return 'Hello, World!'
+
 
 @app.route('/user', methods=['GET', 'POST'])
 def user():
+    # TODO Hachage
     if request.method == 'POST':
         params = request.json
         if ('username' and 'email' and 'password' in params and
-        User.query.filter_by(username=params.get('username')).first() is None and
-        User.query.filter_by(email=params.get('email')).first() is None):
-            newUser = User(username=params.get('username'),
-                            email=params.get('email'),
-                            pseudo=params.get('pseudo'),
-                            password=params.get('password'))
-            db.session.add(newUser)
-            db.session.commit()
-            return 'POST User, OK'
-        return 'POST User, not OK'
+        word.match(params.get('username')) and email.match(params.get('email'))):
+            if (User.query.filter_by(username=params.get('username')).first() is None and
+            User.query.filter_by(email=params.get('email')).first() is None):
+                newUser = User(username=params.get('username'),
+                                email=params.get('email'),
+                                pseudo=params.get('pseudo'),
+                                password=params.get('password'))
+                db.session.add(newUser)
+                db.session.commit()
+                return { 'message' : 'Ok', 'data': newUser }
+            else:
+                return Retour.create_error('Bad Request', 400, ['resource already exists'])
+        else:
+            return Retour.create_error('Bad Request', 400, ['bad parameters'])
+
     elif request.method == 'GET':
         return 'GET User'
--- objects.py
+++ objects.py
@@ -1,48 +1,42 @@
-import json
 from datetime import datetime
 
-class User:
+class Retour:
     def create_user(id: int, username: str, pseudo: str, created_at: datetime, email: str) -> str:
-        return json.dumps({
-            "id": id,
-            "username": username,
-            "pseudo": pseudo
-            "created_at": created_at
-            "email": email #PROPRIETAIRE
-        })
+        return {
+            'id': id,
+            'username': username,
+            'pseudo': pseudo,
+            'created_at': created_at,
+            'email': email #PROPRIETAIRE
+        }
 
-
-class Video:
     def create_video(id: int, source: str, created_at: datetime, views: int, enabled: bool, user: str, format: dict):
-        return json.dumps({
-            "id": id,
-            "source": source,
-            "created_at": created_at
-            "views": views
-            "enabled": enabled
-            "user": user
-            "format": format
-        })
+        return {
+            'id': id,
+            'source': source,
+            'created_at': created_at,
+            'views': views,
+            'enabled': enabled,
+            'user': user,
+            'format': format
+        }
 
-class Token:
     def create_token(token: str, user: str):
-        return json.dumps({
-            "token": token,
-            "user": user,
-        })
+        return {
+            'token': token,
+            'user': user
+        }
 
-class Comment:
     def create_comment(id: int, body: str, user: str):
-        return json.dumps({
-            "id": id,
-            "body": body,
-            "user": user
-        })
+        return {
+            'id': id,
+            'body': body,
+            'user': user
+        }
 
-class Error:
     def create_error(message, code, data):
-        return json.dumps({
-            "message": message,
-            "code": code,
-            "data": data
-        })
+        return {
+            'message': message,
+            'code': code,
+            'data': data
+        }
